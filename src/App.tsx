c2f31dac94f107247e4825ef392c3b2b710354db
Enymariam <jammondt@gmail.com>
2025-05-14 18:42:39 +0300

    Removed redundant components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Gallery from "./components/Gallery";
+import React from "react";
 
 const App: React.FC = () => {
-    const [data, setData] = useState<{ message: string } | null>(null);
-
-    useEffect(() => {
-        axios
-            .get("http://localhost:5000/data")
-            .then((res) => setData(res.data))
-            .catch((err) => console.error("Error fetching data:", err));
-    }, []);
-
     return (
         <div className="App">
-            <h1>{data ? data.message : "Loading"}</h1>
-            <Gallery />
-            <p>Testing CI Pipeline</p>
+            <h1>Sentimental Analysis App</h1>
+
+            <p>Simple frontend developing here</p>
         </div>
     );
 };
