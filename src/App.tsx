3f7b4dcc8238ad73096d46d5114856530bc21b5c
Enymariam <jammondt@gmail.com>
2025-05-14 19:24:53 +0300

    Added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import Card from "./components/Card";
 
 const App: React.FC = () => {
     return (
-        <div className="App">
+        <Container maxWidth="lg" sx={{ paddingTop: 4 }}>
             <Typography variant="h1" sx={{ marginBottom: 2 }}>
                 Sentimental Analysis App{" "}
             </Typography>
             <Card></Card>
-        </div>
+        </Container>
     );
 };
 
