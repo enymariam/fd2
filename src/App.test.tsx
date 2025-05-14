103776e50426379bb754b6789c0b7da47302683d
Enymariam <jammondt@gmail.com>
2025-05-14 18:50:58 +0300

    Remove redundant test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,8 @@
-import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
-import axios from "axios";
 
-jest.mock("axios");
-
-describe("App component", () => {
-    it("Shows message from database if data has loaded", async () => {
-        (axios.get as jest.Mock).mockResolvedValue({
-            data: { message: "Hello world" },
-        });
-
-        render(<App />);
-
-        await waitFor(() => screen.getByText("Hello world"));
-        expect(screen.getByText("Hello world")).toBeInTheDocument();
-    });
+test("näyttää sovelluksen otsikon", () => {
+    render(<App />);
+    const otsikko = screen.getByText(/Sentimental Analysis App/i);
+    expect(otsikko).toBeInTheDocument();
 });
