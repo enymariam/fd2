2428e75e5346cea8e72c0c02ec449ec964bbd928
Enymariam <jammondt@gmail.com>
2025-05-14 19:09:16 +0300

    Added simple card component example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "./components/Card";
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <h1>Sentimental Analysis App</h1>
+            <Card></Card>
         </div>
     );
 };
